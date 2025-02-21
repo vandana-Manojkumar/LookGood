aaacc7315042f5e5fbfb68c0ed381b57da8e9bc5
vandana-Manojkumar <manojvandana89@gmail.com>
2025-02-21 21:22:45 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: "*",
+    origin: "https://look-good.vercel.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
