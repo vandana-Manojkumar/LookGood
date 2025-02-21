fd1b2eb5a06374966dfcd9d3f219e454b49d3826
vandana-Manojkumar <manojvandana89@gmail.com>
2025-02-21 23:19:03 +0530

    Update frontend URL

--- server/controllers/shop/order-controller.js
+++ server/controllers/shop/order-controller.js
@@ -26,8 +26,8 @@ const createOrder = async (req, res) => {
         payment_method: "paypal",
       },
       redirect_urls: {
-        return_url: "http://localhost:5173/shop/paypal-return",
-        cancel_url: "http://localhost:5173/shop/paypal-cancel",
+        return_url: "https://look-good.vercel.app/shop/paypal-return",
+        cancel_url: "https://look-good.vercel.app/shop/paypal-cancel",
       },
       transactions: [
         {
