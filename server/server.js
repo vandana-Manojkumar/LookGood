e44d66564512aea09546546ad6cbd2a204c1c2a9
vandana manojkumar <139863758+vandana-Manojkumar@users.noreply.github.com>
2025-02-21 20:19:21 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,20 +27,7 @@ mongoose
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Cache-Control",
-      "Expires",
-      "Pragma",
-    ],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(cookieParser());
 app.use(express.json());
