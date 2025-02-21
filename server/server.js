937d47abd96c3f865070576cb564fa42d248c160
vandana manojkumar <139863758+vandana-Manojkumar@users.noreply.github.com>
2025-02-21 20:29:11 +0530

    Revert previous server.js changes

--- server/server.js
+++ server/server.js
@@ -27,7 +27,18 @@ mongoose
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  }));
 
 app.use(cookieParser());
 app.use(express.json());
