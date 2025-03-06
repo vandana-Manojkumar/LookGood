03935c061c3ab23c1aa2a5bf6edfdacca8458195
vandana-Manojkumar <manojvandana89@gmail.com>
2025-03-06 16:47:58 +0530

    Minor bug fixes

--- server/controllers/shop/order-controller.js
+++ server/controllers/shop/order-controller.js
@@ -37,8 +37,8 @@ const createOrder = async (req, res) => {
         payment_method: "paypal",
       },
       redirect_urls: {
-        return_url: "http://localhost:5173/shop/paypal-return",
-        cancel_url: "http://localhost:5173/shop/paypal-cancel",
+        return_url: "https://look-good.vercel.app/shop/paypal-return",
+        cancel_url: "https://look-good.vercel.app/shop/paypal-cancel",
       },
       transactions: [
         {
