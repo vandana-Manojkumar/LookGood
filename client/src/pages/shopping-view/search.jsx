1b3291e9c9c00b36821c51adedf6e257b50ebeec
vandana manojkumar <139863758+vandana-Manojkumar@users.noreply.github.com>
2025-03-07 12:46:08 +0530

    Update search.jsx

--- client/src/pages/shopping-view/search.jsx
+++ client/src/pages/shopping-view/search.jsx
@@ -85,7 +85,7 @@ function SearchProducts() {
   console.log(searchResults, "searchResults");
 
   return (
-    <div className="container mx-auto md:px-6 px-4 py-8">
+    <div className=" mt-20 container mx-auto md:px-6 px-4 py-8">
       <div className="flex justify-center mb-8">
         <div className="w-full flex items-center">
           <Input
